cc6bbfa851624fc623309f52bce5704eda9ae0d9
Devops-HCL <131745278+Devops-HCL@users.noreply.github.com>
2023-08-18 14:21:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    name = input("Enter your name: ")
-    age = int(input("Enter your age: "))
-    return f"Hello, {name}! You are {age} years old."
+    name = input("NAME: ")
+    sap_id = int(input("SAP_ID: "))
+    return f"Hello, {name}! Welcome to HCL Technologies,Your sap id {sap_id}."
 
 if __name__ == '__main__':
     app.run()
