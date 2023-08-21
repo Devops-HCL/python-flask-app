e50089a46d5305df814647f044e0927fcb9af1fa
Devops-HCL <131745278+Devops-HCL@users.noreply.github.com>
2023-08-21 16:00:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def main():
     name = input("NAME: ")
     sap_id = int(input("SAP_ID: "))
-    return f"Hello, {name}! Welcome to HCL Technologies,Your sap id {sap_id}."
+    return f"Hello, {name}! Welcome! Your sap id {sap_id}."
 
 if __name__ == '__main__':
     app.run()
